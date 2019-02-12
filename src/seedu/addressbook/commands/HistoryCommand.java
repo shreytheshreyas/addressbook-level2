637e76e7709583a5c5d1ecc96dcc2e4dd1e4266c
suriruhani <suriruhani@gmail.com>
2019-02-12 22:27:19 +0800

    Add logic of HistoryCommand

--- src/seedu/addressbook/commands/HistoryCommand.java
+++ src/seedu/addressbook/commands/HistoryCommand.java
@@ -1,17 +1,24 @@
 package seedu.addressbook.commands;
 
 
+import java.util.Stack;
+
+import static seedu.addressbook.Main.CommandHistory;
+
 public class HistoryCommand extends Command{
 
     public static final String COMMAND_WORD = "history";
     public static final String MESSAGE_USAGE = "Shows all the commands history in reverse chronological order.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Displaying all commands!";
+    public static final String MESSAGE_SUCCESS = "Displaying all past commands!";
 
     @Override
     public CommandResult execute() {
-        System.out.println("Testing history command");
+        Stack<String> CHCopy = CommandHistory;
+        while (!CHCopy.empty()) {
+            System.out.println("|| " + CHCopy.pop());
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
